92fe285b614bc6e2b8631437b28c9c49f98425a3
Allie Carter <ascarter26@gmail.com>
2015-05-27 20:53:58 -0400

    Uploading files
    First attempt

---
+++ js/script.js
@@ -0,0 +1,114 @@
+  $ ('.startbutton').click (function(){
+	$('#q1').slideToggle("slow");
+});
+
+  $ ('#yes1').click (function(){
+	$('#q2').slideToggle("slow");
+});
+
+
+  $ ('#no1').click (function(){
+	$('#q7').slideToggle("slow");
+});
+
+    $ ('#yes2').click (function(){
+	$('#q3').slideToggle("slow");
+});
+
+    $ ('#no2').click (function(){
+	$('#resultN').slideToggle("slow");
+});
+
+    $ ('#yes3').click (function(){
+	$('#q4').slideToggle("slow");
+});
+
+    $ ('#no3').click (function(){
+	$('#resultN').slideToggle("slow");
+});
+
+    $ ('#yes4').click (function(){
+	$('#q5').slideToggle("slow");
+});
+
+    $ ('#no4').click (function(){
+	$('#resultN').slideToggle("slow");
+});
+
+    $ ('#yes5').click (function(){
+	$('#q6').slideToggle("slow");
+});
+
+    $ ('#no5').click (function(){
+	$('#resultN').slideToggle("slow");
+});
+
+    $ ('#yes6').click (function(){
+	$('#q13').slideToggle("slow");
+});
+
+    $ ('#no6').click (function(){
+	$('#resultN').slideToggle("slow");
+});
+
+    $ ('#yes7').click (function(){
+	$('#q8').slideToggle("slow");
+});
+
+    $ ('#no7').click (function(){
+	$('#resultN').slideToggle("slow");
+});
+
+
+    $ ('#yes8').click (function(){
+	$('#q9').slideToggle("slow");
+});
+
+    $ ('#no8').click (function(){
+	$('#resultN').slideToggle("slow");
+});
+
+
+    $ ('#yes9').click (function(){
+	$('#q10').slideToggle("slow");
+});
+
+    $ ('#no9').click (function(){
+	$('#resultN').slideToggle("slow");
+});
+
+
+    $ ('#yes10').click (function(){
+	$('#q13').slideToggle("slow");
+});
+
+    $ ('#no10').click (function(){
+	$('#q11').slideToggle("slow");
+});
+
+
+    $ ('#yes11').click (function(){
+	$('#q13').slideToggle("slow");
+});
+
+    $ ('#no11').click (function(){
+	$('#q12').slideToggle("slow");
+});
+
+
+    $ ('#yes12').click (function(){
+	$('#q13').slideToggle("slow");
+});
+
+    $ ('#no12').click (function(){
+	$('#resultN').slideToggle("slow");
+});
+
+
+    $ ('#yes13').click (function(){
+	$('#resultN').slideToggle("slow");
+});
+
+    $ ('#no13').click (function(){
+	$('#resultY').slideToggle("slow");
+});
